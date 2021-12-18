106c222d7cf6500f3b2b35f7ba242304aeefc975
matthewzareski <78160277+matthewzareski@users.noreply.github.com>
2021-12-18 09:49:37 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 img = "";
+objects = [0];
+
 
 function setup(){
 canvas = createCanvas(640, 420);
@@ -10,13 +12,20 @@ function preload(){
 }
 
 function draw(){
-image(img, 0, 0, 640, 420);
-fill("red");
-text("Dog", 47, 80);
-noFill();
-stroke("red");
-rect(30, 60, 450, 350);
+    image(img, 0, 0, 640, 420);
+
+    if(status1 != ""){
+    for (i = 0; i < objects.length; i++){
+    document.getElementById("status").innerHTML = "Status : Object Detected";
+    
+    fill("#FF0000");
+    percent = floor(objects[i].confidence * 100);
+    text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+    noFill();
+    stroke("#FF0000");
+    rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
 }
+    }
 
 function modelLoaded(){
     console.log("Model Loaded!");
@@ -29,4 +38,5 @@ function gotResult(error, results){
         console.log(error);
     }
     console.log(results);
-}
+    objects = results;
+}   }
